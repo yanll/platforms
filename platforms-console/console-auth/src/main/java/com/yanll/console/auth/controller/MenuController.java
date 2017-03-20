1d5ac7cde4762579b66b9a975c418fd679d717b0
yanll <m3GAGGM7>
2017-03-20 11:22:37 +0800

    M

--- platforms-console/console-auth/src/main/java/com/yanll/console/auth/controller/MenuController.java
+++ platforms-console/console-auth/src/main/java/com/yanll/console/auth/controller/MenuController.java
@@ -4,6 +4,8 @@ package com.yanll.console.auth.controller;
 import com.yanll.console.auth.manager.MenuManager;
 import com.yanll.framework.util.exception.BizCode;
 import com.yanll.framework.web.result.JSON;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +20,17 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping(value = "/menu")
+@Api("菜单管理")
 public class MenuController {
     private static final Logger logger = LoggerFactory.getLogger(MenuController.class);
     @Autowired
     MenuManager menuManager;
 
+    @ApiOperation(value = "查询树形菜单集合", notes = "查询树形菜单集合...")
     @RequestMapping(value = "/tree/{portal_id}", method = RequestMethod.GET, name = "查询树形菜单集合")
     @ResponseBody
     public JSON<List<Map<String, Object>>> tree(@PathVariable Long portal_id) {
-        List<Map<String, Object>> list = menuManager.selectMapTreeMenus(  portal_id);
+        List<Map<String, Object>> list = menuManager.selectMapTreeMenus(portal_id);
         return new JSON(BizCode.OK.getValue(), list);
     }
 
