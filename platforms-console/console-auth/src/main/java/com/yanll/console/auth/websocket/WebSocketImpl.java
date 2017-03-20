fa533cf86d5844dbe74cd2fcfe9ece11488dd9aa
yanll <m3GAGGM7>
2017-03-20 18:55:50 +0800

    M

---
+++ platforms-console/console-auth/src/main/java/com/yanll/console/auth/websocket/WebSocketImpl.java
@@ -0,0 +1,14 @@
+package com.yanll.console.auth.websocket;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by YANLL on 2017/03/20.
+ */
+@ServerEndpoint(value = "/websocket")
+@Component
+public class WebSocketImpl {
+
+}
