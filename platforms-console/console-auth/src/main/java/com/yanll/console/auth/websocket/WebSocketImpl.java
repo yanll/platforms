a4bc636dc48679cc28c42f6580c64ecc4a9be4fd
yanll <m3GAGGM7>
2017-03-28 11:38:50 +0800

    M

--- platforms-console/console-auth/src/main/java/com/yanll/console/auth/websocket/WebSocketImpl.java
+++ platforms-console/console-auth/src/main/java/com/yanll/console/auth/websocket/WebSocketImpl.java
@@ -2,7 +2,13 @@ package com.yanll.console.auth.websocket;
 
 import org.springframework.stereotype.Component;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * Created by YANLL on 2017/03/20.
@@ -10,5 +16,49 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/websocket")
 @Component
 public class WebSocketImpl {
+    private static int onlineCount = 0;
 
+    private static CopyOnWriteArraySet<WebSocketImpl> webSocketSet = new CopyOnWriteArraySet<>();
+
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(this);
+        addOnlineCount();
+        System.out.println("有新链接加入!当前在线人数为" + getOnlineCount());
+    }
+
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);
+        subOnlineCount();
+        System.out.println("有一链接关闭!当前在线人数为" + getOnlineCount());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("来自客户端的消息:" + message);
+        //群发消息
+        for (WebSocketImpl item : webSocketSet) {
+            item.sendMessage(message);
+        }
+    }
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+
+    public static synchronized int getOnlineCount() {
+        return WebSocketImpl.onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        WebSocketImpl.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        WebSocketImpl.onlineCount--;
+    }
 }
